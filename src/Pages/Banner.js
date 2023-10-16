e4a182a20f59af5877435fdde786f70b70f27f63
ellen3128 <hyunpark.ellen@gmail.com>
2023-10-15 22:30:44 -0400

    modifies cover page

--- src/Pages/Banner.js
+++ src/Pages/Banner.js
@@ -1,16 +1,19 @@
-
+import lidGif from '../Img/lid.gif';
 
 export default function Banner() {
   return (
     <section id="Home" className="coverText">
-      <span data-text="Cover,">Cover,</span>
-      <span data-text="your">your</span>
-      <span data-text="drink,">drink,</span>
-      <span data-text="Spill">Spill</span>
-      <span data-text="less,">less,</span>
-      <span data-text="Keep">Keep</span>
-      <span data-text="Warm.">Warm.</span>
-
+      <div className="textContainer">
+        <span data-text="Cover,">Cover,</span>
+        <span data-text="your">your</span>
+        <span data-text="drink,">drink,</span>
+        <span data-text="Spill">Spill</span>
+        <span data-text="less,">less,</span>
+        <span data-text="Keep">Keep</span>
+        <span data-text="Warm.">Warm.</span>
+      </div>
+      <img className="lid" src={lidGif} alt="Lid GIF"></img>
     </section>
   );
 }
+
