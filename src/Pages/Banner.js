ff78dfe9f38c1557a4943cd76f5a603d2c5949f7
ellen3128 <hyunpark.ellen@gmail.com>
2023-10-15 22:10:46 -0400

    updates slogan

--- src/Pages/Banner.js
+++ src/Pages/Banner.js
@@ -1,12 +1,16 @@
-import Img from '../Img/sb_sample.png';
 
-export default function Banner () {
-    return (
-        <section id="Home">
-        <h1> Cover your drink, <br></br>
-        Spill less, <br></br>
-        keep warm. </h1>
-        <img src= {Img} alt= ""></img>
-        </section>
-    )
+
+export default function Banner() {
+  return (
+    <section id="Home" className="coverText">
+      <span data-text="Cover,">Cover,</span>
+      <span data-text="your">your</span>
+      <span data-text="drink,">drink,</span>
+      <span data-text="Spill">Spill</span>
+      <span data-text="less,">less,</span>
+      <span data-text="Keep">Keep</span>
+      <span data-text="Warm.">Warm.</span>
+
+    </section>
+  );
 }
