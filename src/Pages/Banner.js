66db1929a03534cebfbd2ba38a23d0518805c790
ellen3128 <hyunpark.ellen@gmail.com>
2023-10-16 22:20:01 -0400

    update button on banner.js

--- src/Pages/Banner.js
+++ src/Pages/Banner.js
@@ -1,14 +1,32 @@
 import lidGif from "../Img/lid.gif";
+import { Link } from "react-scroll";
+
 
 export default function Banner() {
   return (
-    <section id="Home" className="coverText">
-      <div className="textContainer">
-        <p data-text="Cover your drink,">Cover your drink,</p>
-        <p data-text="Spill less,">Spill less,</p>
-        <p data-text="Keep warm.">Keep warm.</p>
+    <div>
+      <section id="Home" className="coverText">
+        <div className="textContainer">
+          <p data-text="Cover your drink,">Cover your drink,</p>
+          <p data-text="Spill less,">Spill less,</p>
+          <p data-text="Keep warm.">Keep warm.</p>
+        </div>
+        <img className="lid" src={lidGif} alt="Lid GIF"></img>
+      </section>
+      {/* <div class="buttons-container"> */}
+        {/* <button class="button-arounder">View Products</button> */}
+        <div className="buttons-container">
+        <Link
+            spy={true}
+            smooth={true}
+            offset={-70}
+            duration={500}
+            to="Products"
+            className="text-lg font-semibold leading-8 text-gray-900 cursor-pointer px-4 py-3 text-white align-middle"
+          >
+            View Products
+          </Link>
       </div>
-      <img className="lid" src={lidGif} alt="Lid GIF"></img>
-    </section>
+   </div>
   );
 }
