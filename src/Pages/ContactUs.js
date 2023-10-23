77576404e5ccdead1aebca7f3c08d449a59b7fe2
ellen3128 <hyunpark.ellen@gmail.com>
2023-10-22 23:43:58 -0400

    contact page set-up

---
+++ src/Pages/ContactUs.js
@@ -0,0 +1,5 @@
+export default function ContactUs () {
+    return (
+        <h1> CONTACT PATGE </h1>
+    )
+}
--- src/Pages/Homescreen/index.js
+++ src/Pages/Homescreen/index.js
@@ -3,6 +3,7 @@ import Banner from "../Banner";
 import Products from "../../Pages/Products";
 import AboutUs from "../AboutUs";
 import Faq from "../Faq";
+import ContactUs from "../ContactUs";
 
 export default function Home() {
   return (
@@ -12,6 +13,7 @@ export default function Home() {
       <Products />
       <AboutUs />
       <Faq />
+      <ContactUs />
     </>
   );
 }
