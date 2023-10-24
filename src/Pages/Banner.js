e707c102f2184c0fc08e00868f9ebf1379a591f1
ellen3128 <hyunpark.ellen@gmail.com>
2023-10-24 11:35:29 -0400

    modifies view products button path to /customize page

--- src/Pages/Banner.js
+++ src/Pages/Banner.js
@@ -1,5 +1,5 @@
 import lidGif from "../Img/lid.gif";
-import { Link } from "react-scroll";
+import { Link } from "react-router-dom";
 
 export default function Banner() {
   return (
@@ -9,15 +9,13 @@ export default function Banner() {
           <p data-text="Cover your drink,">Cover your drink,</p>
           <p data-text="Spill less,">Spill less,</p>
           <p data-text="Keep warm.">Keep warm.</p>
-          <Link
-            spy={true}
-            smooth={true}
-            offset={-150}
-            duration={500}
-            to="Products"
-            className="buttons-container text-lg font-semibold leading-8 text-gray-900 cursor-pointer px-4 py-3 text-white align-middle font-sans ui-sans-serif"
+          <Link to="/customize">
+          <button
+            type="button"
+            class="buttons-container text-lg font-semibold leading-8 text-gray-900 cursor-pointer px-4 py-3 mt-6 text-white align-middle font-sans ui-sans-serif"
           >
             View Products
+            </button>
           </Link>
         </div>
         <img className="lid" src={lidGif} alt="Lid GIF"></img>
