01a47ce43a0f86fccd41f925b86741f5f107f90f
ellen3128 <hyunpark.ellen@gmail.com>
2023-10-24 11:50:57 -0400

    adds contents to be added on the customize page

--- src/Pages/Customize.js
+++ src/Pages/Customize.js
@@ -1,5 +1,11 @@
 export default function Customize () {
     return (
-        <h1> HIIII </h1>
+        <>
+        <p className="text-center mx-auto w-3/4 text-5xl p-20 font-extrabold"> PRODUCT DETAILS PAGE </p>
+        <p className="text-center mx-auto w-3/4 text-2xl pt-5 font-extrabold"> 1. AAU, I want to view and choose product categories of lids and pieces </p>
+        <p className="text-center mx-auto w-3/4 text-2xl pt-5 font-extrabold"> 2. AAU, I want to customize and preview customized pieces with color options </p>
+        <p className="text-center mx-auto w-3/4 text-2xl pt-5 font-extrabold"> 3. AAU, I want to request a sample of customized products </p>
+        <p className="text-center mx-auto w-3/4 text-2xl pt-5 font-extrabold"> 4. AAU, I want to submit an order by selecting product categories and quantities </p>
+        </>
     )
 }
